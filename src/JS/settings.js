795191a05628d9f55b6209cc9a54ab2a166162b5
David Olivero <74565558+DavidOlivero@users.noreply.github.com>
2023-07-18 14:32:11 -0500

    WhatSapp option added

--- src/JS/settings.js
+++ src/JS/settings.js
@@ -1,8 +1,25 @@
 const { invoke } = window.__TAURI__.tauri
 
 $(document).ready(() => {
+    const theme = $("#theme")
+    const WhatSapp = $("#WhatSapp")
+
+    const theme_option = $("li:contains('Apariencia')")
     const light_button = $("#light")
     const dark_button = $("#dark")
+
+    const WhatSapp_option = $("li:contains('WhatSapp')")
+    const add_number_input = $("#add-contact input")
+    
+    theme_option.click( function () {
+        theme.show()
+        WhatSapp.hide()
+    })
+
+    WhatSapp_option.click( function () {        
+        WhatSapp.show()
+        theme.hide()
+    })
     
     light_button.click(() => {
         localStorage.setItem("Theme", "light")
