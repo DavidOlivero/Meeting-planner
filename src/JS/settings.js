a800f0490cfb63be88a0337790181cd5905223cc
David Olivero <74565558+DavidOlivero@users.noreply.github.com>
2023-07-20 15:59:05 -0500

    Finishing some WhatSapp functions - add contact numbers -

--- src/JS/settings.js
+++ src/JS/settings.js
@@ -1,4 +1,35 @@
 const { invoke } = window.__TAURI__.tauri
+let number = JSON.parse(localStorage.getItem("Contacts")) || []
+
+const save_contact = () => {
+    const add_number_input = $("#contact input")
+    
+    if(add_number_input.val()) {
+        number.push(add_number_input.val())
+        add_number_input.val("")
+        localStorage.setItem("Contacts", JSON.stringify(number))
+        number = JSON.parse(localStorage.getItem("Contacts"))
+
+        render_contacts()
+    }
+}
+
+const render_contacts = () => {
+    const number_list = $("#number-list")
+    number_list.empty()
+
+    number.forEach(element => {
+        number_list.append(`<li>${element} <i class="fa-solid fa-trash-can"></i></li>`)
+    })
+
+    $(document).on("mouseenter", "#number-list li", function () {
+        $(this).find(".fa-trash-can").show()
+    })
+
+    $(document).on("mouseleave", "#number-list li", function () {
+        $(this).find(".fa-trash-can").hide()
+    })
+}
 
 $(document).ready(() => {
     const theme = $("#theme")
@@ -9,7 +40,6 @@ $(document).ready(() => {
     const dark_button = $("#dark")
 
     const WhatSapp_option = $("li:contains('WhatSapp')")
-    const add_number_input = $("#add-contact input")
     
     theme_option.click( function () {
         theme.show()
@@ -30,4 +60,24 @@ $(document).ready(() => {
         localStorage.setItem("Theme", "dark")
         location.reload()
     })
+
+    $(".fa-plus").click(() => {
+        save_contact()
+    })
+
+    $(document).keydown((event) => {
+        if (event.keyCode === 13) {
+            save_contact()
+        }
+    })
+
+    $(document).on("click", ".fa-trash-can", function () {
+        const text = $(this).closest("li").text().trim()
+        const index = number.indexOf(text)
+        number.splice(index, 1)
+        localStorage.setItem("Contacts", JSON.stringify(number))
+        render_contacts()
+    })
+
+    render_contacts()
 })
