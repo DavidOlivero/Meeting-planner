6e3520a4dec61054145a0ad2854aa70d6cec0930
David Olivero <daniolidavid32@gmail.com>
2023-08-25 13:15:31 -0500

    Allow to the user add the contact name

--- src/JS/settings.js
+++ src/JS/settings.js
@@ -3,14 +3,29 @@ let number = JSON.parse(localStorage.getItem("Contacts")) || []
 
 const save_contact = () => {
     const add_number_input = $("#contact input")
+    const add_name = $("#add-name")
+    const name_input = $("#add-name input")
     
     if(add_number_input.val()) {
-        number.push(add_number_input.val())
-        add_number_input.val("")
-        localStorage.setItem("Contacts", JSON.stringify(number))
-        number = JSON.parse(localStorage.getItem("Contacts"))
+        add_name.css("visibility", "visible")
 
-        render_contacts()
+        $("#send").click(() => {
+            console.log(name_input)
+            if (name_input.val()) {
+                number.push(add_number_input.val() + "/" + name_input.val())
+
+                add_number_input.val("")
+                localStorage.setItem("Contacts", JSON.stringify(number))
+                number = JSON.parse(localStorage.getItem("Contacts"))
+
+                name_input.val("")
+                add_name.css("visibility", "hidden")
+
+                render_contacts()
+            } else {
+                alert("Debe agregarle un nombre al contacto")
+            }
+        })
     }
 }
 
@@ -19,15 +34,17 @@ const render_contacts = () => {
     number_list.empty()
 
     number.forEach(element => {
-        number_list.append(`<li>${element} <i class="fa-solid fa-trash-can"></i></li>`)
+        const info = element.split("/")
+
+        number_list.append(`<li>${info[1]} <div><i class="fa-solid fa-file-pen"></i> <i class="fa-solid fa-trash-can"></i></div></li>`)
     })
 
     $(document).on("mouseenter", "#number-list li", function () {
-        $(this).find(".fa-trash-can").show()
+        $(this).find("i").show()
     })
 
     $(document).on("mouseleave", "#number-list li", function () {
-        $(this).find(".fa-trash-can").hide()
+        $(this).find("i").hide()
     })
 }
 
